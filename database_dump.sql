e5d5c6a4df2ac84873ad59f7700bf61947d81040
dodepunia2002 <220030750@stikom-bali.ac.id>
2026-01-16 13:51:16 +0800

    feat: reset database and seed initial data with users and candidates

--- database_dump.sql
+++ database_dump.sql
@@ -2,25 +2,25 @@ PRAGMA foreign_keys=OFF;
 BEGIN TRANSACTION;
 
 -- 1. TABEL USERS (Anggota Kelompok)
-CREATE TABLE IF NOT EXISTS "users" (
+-- Password untuk semua akun: '123456'
+DROP TABLE IF EXISTS "users";
+CREATE TABLE "users" (
     "id" INTEGER PRIMARY KEY AUTOINCREMENT,
-    "email" VARCHAR,
+    "email" VARCHAR UNIQUE,
     "username" VARCHAR,
     "password" VARCHAR,
     "is_active" BOOLEAN
 );
 
--- Password untuk semua akun adalah: '123456'
--- Hash: $2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxh96pPrEOOeE31E32GVgBnGdOHnu
+INSERT INTO users VALUES(1,'agung@example.com','gungdanan2005-maker','$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxh96pPrEOOeE31E32GVgBnGdOHnu',1);
+INSERT INTO users VALUES(2,'yoga@example.com','Yoga240030457','$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxh96pPrEOOeE31E32GVgBnGdOHnu',1);
+INSERT INTO users VALUES(3,'zacky@example.com','ZCASTUDIO','$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxh96pPrEOOeE31E32GVgBnGdOHnu',1);
+INSERT INTO users VALUES(4,'dodepunia@example.com','dodepunia2002','$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxh96pPrEOOeE31E32GVgBnGdOHnu',1);
+INSERT INTO users VALUES(5,'anom@example.com','AnomWibawa','$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxh96pPrEOOeE31E32GVgBnGdOHnu',1);
 
-INSERT INTO users VALUES(1,'agung@example.com','gungdanan2005-maker','b',1);
-INSERT INTO users VALUES(2,'yoga@example.com','Yoga240030457','b',1);
-INSERT INTO users VALUES(3,'zacky@example.com','ZCASTUDIO','b',1);
-INSERT INTO users VALUES(4,'dodepunia@example.com','dodepunia2002','b',1);
-INSERT INTO users VALUES(5,'anom@example.com','AnomWibawa','b',1);
-
--- 2. TABEL CANDIDATES (Calon Ketua)
-CREATE TABLE IF NOT EXISTS "candidates" (
+-- 2. TABEL CANDIDATES
+DROP TABLE IF EXISTS "candidates";
+CREATE TABLE "candidates" (
     "id" INTEGER PRIMARY KEY AUTOINCREMENT,
     "name" VARCHAR,
     "description" VARCHAR
@@ -30,19 +30,18 @@ INSERT INTO candidates VALUES(1,'Kandidat 01: Budi Santoso','Mewujudkan organisa
 INSERT INTO candidates VALUES(2,'Kandidat 02: Siti Aminah','Meningkatkan kreativitas dan kolaborasi antar anggota melalui seni & budaya.');
 INSERT INTO candidates VALUES(3,'Kandidat 03: Andi Pratama','Fokus pada pengembangan kewirausahaan (Entrepreneurship) mahasiswa.');
 
--- 3. TABEL VOTES (Contoh Suara Masuk)
-CREATE TABLE IF NOT EXISTS "votes" (
+-- 3. TABEL VOTES
+DROP TABLE IF EXISTS "votes";
+CREATE TABLE "votes" (
     "id" INTEGER PRIMARY KEY AUTOINCREMENT,
-    "user_id" INTEGER,
+    "user_id" INTEGER UNIQUE,
     "candidate_id" INTEGER,
     FOREIGN KEY(user_id) REFERENCES users(id),
     FOREIGN KEY(candidate_id) REFERENCES candidates(id)
 );
 
--- Simulasi: Agung (ID 1) memilih Kandidat 1
+-- Simulasi: Agung & Yoga sudah memilih
 INSERT INTO votes VALUES(1,1,1);
-
--- Simulasi: Yoga (ID 2) memilih Kandidat 2
 INSERT INTO votes VALUES(2,2,2);
 
 COMMIT;
